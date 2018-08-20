b72ba2fe8c0a06823717237c11aedd89ac081fa4
dereje kitaw <dereje.kitaw@gmail.com>
2018-08-20 15:43:31 -0400

    initial

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import { storeCreator, createStore } from "redux";
+
+const store = createStore((state = { count: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      count: state.count + 1
+    };
+  } else return state;
+});
+
+console.log(store.getState());
+
+store.dispatch({
+  type: "INCREMENT"
+});
+
+store.dispatch({
+  type: "INCREMENT"
+});
+
+console.log(store.getState());
